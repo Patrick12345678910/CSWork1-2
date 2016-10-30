e41666d878edfec89ea7406ef111a624f5f43881
Patrick12345678910 <pwright.63845@farnborough.ac.uk>
2016-10-30 16:06:26 +0000

    Add files via upload

---
+++ wordGuess.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+import java.util.*;
+import java.util.concurrent.TimeUnit;
+public class wordGuess 
+{
+	public static void main (String[ ] args){
+		Scanner input = new Scanner(System.in);
+		System.out.println("In this game, you will be required to guess the six letter word, please enter your first guess. ");
+		String playerGuess = input.nextLine();
+		playerGuess.toUpperCase();
+		String Corans = ("RANDOM");
+		if (playerGuess.equals(Corans));{
+			System.out.println("Correct, well done");
+		}
+		 
+	}
+
+	private static void toUpperCase() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private static void While(boolean equals) {
+		// TODO Auto-generated method stub
+		
+	}
+}
